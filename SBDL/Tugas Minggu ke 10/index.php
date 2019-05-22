eb8f6829cfd058db2c9ced30839b5764bc1b3776
Nugraha Sugiana Ridwan <18566130+nu-gee@users.noreply.github.com>
2019-05-22 20:57:14 +0700

    Penambahan Index.php
    Index.php yang berfungsi sebagai main menu utama di aplikasi puskesmas ini.

---
+++ SBDL/Tugas Minggu ke 10/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>DATABASE PUSKESMAS</title>
+	<style type="text/css">
+		#kiri {
+			width: 50%;
+			float: left;
+		}
+
+		#kanan {
+			width: 50%;
+			float: right;
+		}
+	</style>
+</head>
+
+<body>
+	<header>
+		<h3 align=center><a href="index.php">Home</a></h3>
+		<h1 align=center>Data Puskesmas</h1>
+	</header>
+
+	<h4 align=center>Menu</h4>
+	<nav align=center>
+		<div id="kiri">
+			<p align=center><a href="dokter\form_dokter.php">Input Data Dokter Baru</a></p>
+			<p align=center><a href="perawat\form_perawat.php">Input Data Perawat Baru</a></p>
+			<p align=center><a href="pasien\form_pasien.php">Input Data Pasien Baru</a></p>
+			<p align=center><a href="ruangan\form_ruangan.php">Input Data Ruangan</a></p>
+			<p align=center><a href="penyakit\form_penyakit.php">Input Data Penyakit</a></p>
+			<p align=center><a href="obat\form_obat.php">Input Data Obat</a></p>
+			<p align=center><a href="rawat\form_rawat.php">Input Data Perawatan Baru</a></p>
+		</div>
+		<div id="kanan">
+			<p align=center><a href="dokter\tabel_dokter.php">Tabel Dokter</a></p>
+			<p align=center><a href="perawat\tabel_perawat.php">Tabel Perawat</a></p>
+			<p align=center><a href="pasien\tabel_pasien.php">Tabel Pasien</a></p>
+			<p align=center><a href="ruangan\tabel_ruangan.php">Tabel Ruangan</a></p>
+			<p align=center><a href="penyakit\tabel_penyakit.php">Tabel Penyakit</a></p>
+			<p align=center><a href="obat\tabel_obat.php">Tabel Obat</a></p>
+			<p align=center><a href="rawat\tabel_rawat.php">Tabel Perawatan</a></p>
+		</div>
+	</nav>
+</body>
+
+</html>
